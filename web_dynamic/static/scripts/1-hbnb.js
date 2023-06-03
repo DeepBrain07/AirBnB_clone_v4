d6667ff9391b38741a4c5a0db8765458887f292d
Timilehin Olusa <olusatimmie07@gmail.com>
2023-06-03 15:07:51 +0100

    Debugging

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+	
+	const amenityList = {};
+	$("li input[type=checkbox]").change(
+		function(){
+		if (this.checked){
+			amenityList[this.dataset.name] = this.dataset.id;
+		} else { 
+			delete amenityList[this.dataset.name];
+		}
+		$(".amenities h4").text(Object.keys(amenityList).sort().join(", "));
+		});
+		});
