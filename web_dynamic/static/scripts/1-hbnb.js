9335c46951ccf2788f4dcb77465acdaec15c30cf
TSOlami <tijani.saheed@yahoo.com>
2023-06-03 11:37:31 +0100

    testing html

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,5 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	/*if ($("input#data-id").prop("checked")){
 		prompt("YEA");
 	}*/
-	$("input").html(<p>hekko</p>);
-})
+	$("input[type=checkbox]").change(
+		function(){
+		if (data_id.checked){
+			alert("checked")
+			}
+		});
