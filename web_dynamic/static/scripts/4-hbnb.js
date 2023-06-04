563a22e35401360cb97a1fc2b760c03ac1fcd854
Timilehin Olusa <olusatimmie07@gmail.com>
2023-06-04 17:49:30 +0100

    updating

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+	const amenityList = {};
+	$("li input[type=checkbox]").change(
+		function(){
+		if (this.checked){
+			amenityList[this.dataset.name] = this.dataset.id;
+		} else { 
+			delete amenityList[this.dataset.name];
+		}
+		$(".amenities h4").text(Object.keys(amenityList).sort().join(", "));
+		});
+	$.getJSON("http://127.0.0.1:5001/api/v1/status/", (data) => {
+		if (data.status === "OK") {
+			$("div#api_status").addClass("available");
+		} else {
+			$("div#api_status").removeClass("available");
+		}
+	});
+	$.ajax({url: "http://127.0.0.1:5001/api/v1/places_search/",
+		type: 'POST',
+		data: JSON.stringify({}),
+		contentType: 'application/json',
+		success: function(response) {
+			res = response['0'];
+			$(".price_by_night").html(res.price_by_night);
+			$(".max_guest").html(res.max_guest);
+			$(".number_rooms").html(res.number_rooms);
+			$(".number_bathrooms").html(res.number_bathrooms);
+			$("div.description").html(res.description);
+		},
+		error: function(error) {
+			console.log("ERROR");
+		}
+	});
+	$("section button").click(function() {
+		$.ajax(url: "http://127.0.0.1:5001/api/v1/places_search/",
+			type: 'POST',
+			data: JSON.stringify(amenityList),
+			contentType: 'application/json');
+	};
+
+});
